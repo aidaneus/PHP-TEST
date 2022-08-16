18aa133a390dcc8e8dedd1ab5d98510d2f0e498b
nhildred <aida_aida29@list.ru>
2022-08-17 00:49:47 +0300

    change value

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 require_once 'classes/request.php';
 
 $request = new request();
-$request->connect = new mysqli("localhost","root","1525","test"); //host,user,password,database - подключение к серверу
+$request->connect = new mysqli("localhost","root","","test"); //host,user,password,database - подключение к серверу
 if (!$request->connect){
     die('Ошибка');
 }
